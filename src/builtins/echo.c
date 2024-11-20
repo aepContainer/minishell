4f451b52086b902f784d75fb7a6bdaf8a0fcbaee
Yunus Emre Özdemir <yunusemreozdemir@yunozdem.local>
2024-11-20 03:16:56 +0300

    duzenlemeler bitti

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,38 +1,12 @@
 #include "../../inc/minishell.h"
 
-static void	echo_line_helper(char *str, bool *in_quote, char *quote_type)
-{
-	int		j;
-
-	j = -1;
-	while (str[++j])
-	{
-		if (ft_strchr(QUOTES, str[j]))
-		{
-			if (!*in_quote)
-			{
-				*quote_type = str[j];
-				*in_quote = true;
-			}
-			else if (str[j] == *quote_type)
-				*in_quote = false;
-			else if (*in_quote)
-				ft_putchar_fd(str[j], 1);
-		}
-		else
-			ft_putchar_fd(str[j], 1);
-	}
-}
 
 void	echo(t_job *job)
 {
 	int		i;
 	int		newline;
-	bool	in_quote;
-	char	quote_type;
 
 	newline = 1;
-	in_quote = false;
 	i = 1;
 	if (job->args[1] && !ft_strncmp(job->args[1], "-n", 2)
 		&& ft_strlen(job->args[1]) == 2)
@@ -42,7 +16,7 @@ void	echo(t_job *job)
 	}
 	while (job->args[i])
 	{
-		echo_line_helper(job->args[i], &in_quote, &quote_type);
+		ft_putstr_fd(job->args[i], 1);
 		if (job->args[i + 1])
 			ft_putchar_fd(' ', 1);
 		i++;
